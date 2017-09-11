afda59529d7d6c0b18e8066163d778c09120f6f7
amarraja123 <amar.arawat@gmail.com>
2017-09-10 22:59:18 -0400

    Refactored Routing module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,28 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
 import {ProductListComponent} from './products.component';
 import {ProductDetailComponent} from './product-detail.component';
 import {ConvertToSpacePipe} from './convertToSpace-pipe';
-import {StarComponent} from './star/star.component';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 import {ProductGuardService} from './product-guard.service';
 import {ProductService} from './products.service';
+import {SharedModule} from '../shared/shared/shared.module';
+import {ProductRoutingModule} from './product-routing/product-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([
-    {path: 'products', component: ProductListComponent},
-    {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductGuardService]}
-    ])
+    SharedModule,
+    ProductRoutingModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacePipe,
-    StarComponent
+    ConvertToSpacePipe
   ],
   providers: [
     ProductService,
